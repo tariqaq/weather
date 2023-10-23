db9b0cbb8cbfbf993037d4dd81522c5bb0f0dcab
tariqaq <trqphoenix@gmail.com>
2023-10-23 19:30:43 +0300

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import *
+
+def main():
+    cls()
+    
+    print('---WEATHER FORECAST---')
+    
+    print('1. Show weather\n'
+          '2. \n')
---
+++ utils.py
@@ -0,0 +1,7 @@
+import os
+
+def cls():
+    if os.name == 'nt':  # nt based system
+        os.system('cls')
+    else:
+        os.system('clear')  # unix based system
