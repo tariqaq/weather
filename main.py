98fe30dd49b92b347017fd81b8b2d38b885814ca
tariqaq <trqphoenix@gmail.com>
2023-10-23 20:29:25 +0300

    spaces1

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ def main():
         
         if usrChoice == 1:
             cls()
+            
             print('weather.') # func call placeholder
+            
             input('Press ENTER to go back')
             cls()
         elif usrChoice == 2 :
             cls()
+            
             print('saved datas.') # func call placeholder
+            
             input('Press ENTER to go back')
             cls()
         elif usrChoice == 3 :
