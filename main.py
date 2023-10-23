8207d37421edda9cee01d8ad58dba4b35305734b
tariqaq <trqphoenix@gmail.com>
2023-10-23 20:26:36 +0300

    menu basic

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from utils import *
 
 def main():
-    cls()
     
-    print('---WEATHER FORECAST---')
+    while True:
+        cls()
+        
+        print('---WEATHER FORECAST---')
+        
+        print('1. Show weather\n'
+            '2. Saved Data\n'
+            '3. Exit Program')
+        
+        usrChoice = int(input('Enter choice : '))
+        
+        if usrChoice == 1:
+            cls()
+            print('weather.')
+            input('Press ENTER to go back')
+            cls()
+        elif usrChoice == 2 :
+            cls()
+            print('saved datas.')
+            input('Press ENTER to go back')
+            cls()
+        elif usrChoice == 3 :
+            print('---Exiting Program---')
+            break
     
-    print('1. Show weather\n'
-          '2. \n')
+    
+main()
